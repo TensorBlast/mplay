8ba1398404cc584026ec985554ef88d4a01dbd9c
ankit <ankitpasi1@gmail.com>
2015-06-13 15:21:28 +0400

    added fullscreen
    have to fix threaded cursor hide and buttonbar hide

--- mplay.py
+++ mplay.py
@@ -5,7 +5,12 @@ from ttk import Frame
 from tkFileDialog import askopenfilename, askopenfile
 from tkMessageBox import showerror, showinfo
 from subprocess import *
+import time
+from threading import Timer, Lock
 
+
+lock = Lock()
+buttonlock = Lock()
 
 class mainframe(Frame):
 
@@ -16,41 +21,96 @@ class mainframe(Frame):
         self.parent = parent
         self.initplayer()
         self.player_process = None
+        self.fstate = False
+        self.paused = True
+        self.trackmouse = True
 
     def initplayer(self):
-        self.videoFrame = Frame(self, width=800, height=480)
+        self.parentframe = Frame(self)
+        self.parentframe.pack(fill=BOTH, expand=True)
+        self.videoFrame = Frame(self.parentframe, width=800, height=480)
         self.videoFrame.pack(side="top", fill="both", expand=True)
-        self.buttonframe = Frame(self, padding="2 2 11 11")
-        self.buttonframe.pack(side="bottom", fill="x", expand=True)
+        self.buttonframe = Frame(self.parentframe, padding="2 2 1 1")
+        self.buttonframe.pack(side="bottom", fill="x", expand=False)
 
         self.selectbutton = Button(self.buttonframe, text="Select")
         self.selectbutton.grid(column=0, row=0, sticky=[E,W])
-        self.playbutton = Button(self.buttonframe, text="Play").grid(column=1, row=0, sticky=[E,W])
+        self.playbutton = Button(self.buttonframe, text="Play")
+        self.playbutton.config(command=self.playpause)
+        self.playbutton.grid(column=1, row=0, sticky=[E,W])
+        self.fullscreenbutton = Button(self.buttonframe, text="Fullscreen", command=self.togglefullscreen)
+        self.fullscreenbutton.grid(column=2, row=0, sticky=[E,W])
         for child in self.buttonframe.winfo_children(): child.grid_configure(padx=5, pady=5)
         self.buttonframe.rowconfigure(0, weight=1)
         self.buttonframe.columnconfigure(0, weight=1)
         self.buttonframe.columnconfigure(1, weight=1)
+        self.buttonframe.columnconfigure(2, weight=1)
 
-        self.selectbutton.configure(command = self.fileopen)
+        self.selectbutton.configure(command=self.fileopen)
         self.videoFrame.bind("<Button-1>",self.playpause)
+        self.parent.bind("<F11>", self.togglefullscreen)
+        self.parent.bind("<Motion>",self.mouseops)
+
+    def mouseops(self,event=None):
+        with lock:
+            self.videoFrame.config(cursor="")
+        Timer(5, self.cursorhandler, ()).start()
+
+        if self.trackmouse:
+            x, y = self.parent.winfo_pointerx(), self.parent.winfo_pointery()
+            windowx, windowy = self.parent.winfo_width(), self.parent.winfo_height()
+            if self.fstate and (windowy - 30 <= y):
+                with buttonlock:
+                    self.buttonframe.pack(side="bottom", fill="x", expand=False)
+                    self.trackmouse = False
+                    Timer(5, self.mousetracker, ()).start()
+            elif self.fstate:
+                with buttonlock:
+                    self.buttonframe.pack_forget()
+
+    def mousetracker(self):
+        print 'Mouse Tracker'
+        with buttonlock:
+            self.trackmouse = True
+
+    def cursorhandler(self):
+        with lock:
+            self.videoFrame.config(cursor="none")
+
+    def togglefullscreen(self, event=None):
+        self.fstate = not self.fstate
+        self.parent.attributes("-fullscreen",self.fstate)
+        if self.fstate:
+            self.buttonframe.pack_forget()
+            self.videoFrame.config(cursor="none")
+        else:
+            self.buttonframe.pack(side="bottom", fill="x", expand=False)
+            Timer(5, self.cursorhandler, ()).start()
 
     def fileopen(self):
-        self.filenm = self.filenm=askopenfilename(filetypes=[("Supported Files","*.mp4;*.mkv;*.mpg;*.avi")])
+        self.filenm = self.filenm=askopenfilename(filetypes=[("Supported Files","*.mp4;*.mkv;*.mpg;*.avi;*.mov")])
         self.play()
 
     def play(self):
         if self.filenm is not None and self.filenm != "":
             winid = self.videoFrame.winfo_id()
             if self.mplayer_isrunning():
-                self.player_process.kill()
+                self.stop()
             try:
-                self.player_process = Popen(["mplayer","-slave","-quiet","-wid",str(winid),self.filenm],stdin=PIPE, stdout=PIPE)
+                self.paused = False
+                self.playbutton.configure(text="Pause")
+                self.player_process = Popen(["mplayer","-fs","-slave","-quiet","-wid",str(winid),self.filenm],stdin=PIPE, stdout=PIPE)
             except:
                 showerror("Error","".join(["Couldn't play video:\n",str(sys.exc_info()[1]),str(sys.exc_info()[2])]))
 
-    def playpause(self, event):
+    def playpause(self, event=None):
         if self.player_process is None:
             return
+        self.paused = not self.paused
+        if self.paused:
+            self.playbutton.configure(text="Play")
+        else:
+            self.playbutton.configure(text="Pause")
         self.command_player("pause")
 
     def setwh(self,w,h):
@@ -62,7 +122,7 @@ class mainframe(Frame):
 
     def mplayer_isrunning(self):
         if self.player_process is not None:
-            return (self.player_process.poll() is  None)
+            return (self.player_process.poll() is None)
         else:
             return False
 
